37031a7408f24e4c6ddb3a647a420610527143ee
DziadDawid <53613664+DziadDawid@users.noreply.github.com>
2020-08-20 14:58:22 +0200

    useContext and useReducer example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MovieList from "./MovieList";
+import NonPropsComponent from "./NonPropsComponent";
+import { MovieListProvider } from "./MovieListContext";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <MovieListProvider>
+            <div className="App">
+                <NonPropsComponent />
+                <MovieList />
+            </div>
+        </MovieListProvider>
+    );
 }
 
 export default App;
